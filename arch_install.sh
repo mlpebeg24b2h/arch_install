ca4a1bb1546a80bfc4ea65d5009fb1f64b64c405
mlpebeg24b2h <47012505+mlpebeg24b2h@users.noreply.github.com>
2023-07-21 15:48:23 +0200

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -126,7 +126,7 @@ fi
 echo "OK"
 
 printf "STEP 06 - Generate fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab 2> ${error_log}
 rc=$?
 if [ $rc -gt 0 ] ; then
    echo "KO !"
@@ -135,52 +135,140 @@ if [ $rc -gt 0 ] ; then
 fi
 echo "OK"
 
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+printf "STEP 07 - Generate local time zone..."
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime 2> ${error_log}
+rc=$?
+if [ $rc -gt 0 ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+echo "OK"
 
-arch-chroot /mnt hwclock --systohc
+printf "STEP 08 - Set the system time..."
+arch-chroot /mnt hwclock --systohc 2> ${error_log}
+rc=$?
+if [ $rc -gt 0 ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+echo "OK"
 
-sed -i 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g' /mnt/etc/locale.gen
+printf "STEP 09 - Generate locales..."
+max_cr=0
+sed -i 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/g' /mnt/etc/locale.gen 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /mnt/etc/locale.gen 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+arch-chroot /mnt locale-gen 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+cp ./etc/locale.conf /mnt/etc/locale.conf 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+echo "OK"
 
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /mnt/etc/locale.gen
+printf "STEP 10 - Generate keyboard mappings..."
+cp ./etc/vconsole.conf /mnt/etc/vconsole.conf 2> ${error_log}
+rc=$?
+if [ $rc -gt 0 ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+echo "OK"
 
-arch-chroot /mnt locale-gen
+printf "STEP 11 - Generate hostname..."
+cp ./etc/hostname /mnt/etc/hostname 2> ${error_log}
+rc=$?
+if [ $rc -gt 0 ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+echo "OK"
 
-cp ./etc/locale.conf /mnt/etc/locale.conf
+printf "STEP 12 - Configure Networking..."
+max_cr=0
+cp ./etc/NetworkManager/conf.d/dns-servers.conf /mnt/etc/NetworkManager/conf.d/dns-servers.conf 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+cp ./etc/systemd/network/20-ethernet.network /mnt/etc/systemd/network/20-ethernet.network 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+cp ./etc/systemd/network/20-wlan.network /mnt/etc/systemd/network/20-wlan.network 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+cp ./etc/systemd/network/20-wwan.network /mnt/etc/systemd/network/20-wwan.network 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+echo "OK"
 
-cp ./etc/vconsole.conf /mnt/etc/vconsole.conf
-
-cp ./etc/hostname /mnt/etc/hostname
-
-cp ./etc/NetworkManager/conf.d/dns-servers.conf /mnt/etc/NetworkManager/conf.d/dns-servers.conf
-
-cp ./etc/systemd/network/20-ethernet.network /mnt/etc/systemd/network/20-ethernet.network
-
-cp ./etc/systemd/network/20-wlan.network /mnt/etc/systemd/network/20-wlan.network
-
-cp ./etc/systemd/network/20-wwan.network /mnt/etc/systemd/network/20-wwan.network
-
-arch-chroot /mnt pacman -S grub efibootmgr
-
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 fsck.mode=skip"/g' /mnt/etc/default/grub
-
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+printf "STEP 13 - Configure GRUB..."
+max_cr=0
+arch-chroot /mnt pacman -S grub efibootmgr 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 fsck.mode=skip"/g' /mnt/etc/default/grub 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg 2> ${error_log}
+rc=$?
+if [ $rc -gt ${max_cr} ] ; then
+   echo "KO !"
+   echo "ERROR : $(cat ${error_log})"
+   exit
+fi
+echo "OK"
 
 echo "################# END installation script for cyclopia #################"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
