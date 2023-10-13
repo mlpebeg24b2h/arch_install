f49c2e1928d9f272238fd7dbd9d6a7369e05a39a
mlpebeg24b2h <47012505+mlpebeg24b2h@users.noreply.github.com>
2023-10-13 13:53:14 +0200

    change dotfiles location

--- 3-dotfiles.sh
+++ 3-dotfiles.sh
@@ -73,11 +73,11 @@ echo ""
 _installSymLink alacritty ~/.config/alacritty ${WORKSPACE_DOTFILES}/dotfiles/alacritty/ ~/.config
 _installSymLink ranger ~/.config/ranger ${WORKSPACE_DOTFILES}/dotfiles/ranger/ ~/.config
 _installSymLink vim ~/.config/vim ${WORKSPACE_DOTFILES}/dotfiles/vim/ ~/.config
-_installSymLink nvim ~/.config/nvim ${WORKSPACE_DOTFILES}      /dotfiles/nvim/ ~/.config
-_installSymLink starship ~/.config/starship.toml ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-_installSymLink rofi ~/.config/rofi ~/dotfiles/rofi/ ~/.config
-_installSymLink dunst ~/.config/dunst ~/dotfiles/dunst/ ~/.config
-_installSymLink wal ~/.config/wal ~/dotfiles/wal/ ~/.config
+_installSymLink nvim ~/.config/nvim ${WORKSPACE_DOTFILES}/dotfiles/nvim/ ~/.config
+_installSymLink starship ~/.config/starship.toml ${WORKSPACE_DOTFILES}/dotfiles/starship/starship.toml ~/.config/starship.toml
+_installSymLink rofi ~/.config/rofi ${WORKSPACE_DOTFILES}/dotfiles/rofi/ ~/.config
+_installSymLink dunst ~/.config/dunst ${WORKSPACE_DOTFILES}/dotfiles/dunst/ ~/.config
+_installSymLink wal ~/.config/wal ${WORKSPACE_DOTFILES}/dotfiles/wal/ ~/.config
 wal -i screenshots/
 echo "Pywal templates initiated!"
 echo ""
@@ -86,31 +86,31 @@ echo "-> Install GTK dotfiles"
 echo "-------------------------------------"
 echo ""
 
-_installSymLink .gtkrc-2.0 ~/.gtkrc-2.0 ~/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
-_installSymLink gtk-3.0 ~/.config/gtk-3.0 ~/dotfiles/gtk/gtk-3.0/ ~/.config/
-_installSymLink .Xresouces ~/.Xresources ~/dotfiles/gtk/.Xresources ~/.Xresources
-_installSymLink .icons ~/.icons ~/dotfiles/gtk/.icons/ ~/
+_installSymLink .gtkrc-2.0 ~/.gtkrc-2.0 ${WORKSPACE_DOTFILES}/dotfiles/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+_installSymLink gtk-3.0 ~/.config/gtk-3.0 ${WORKSPACE_DOTFILES}/dotfiles/gtk/gtk-3.0/ ~/.config/
+_installSymLink .Xresouces ~/.Xresources ${WORKSPACE_DOTFILES}dotfiles/gtk/.Xresources ~/.Xresources
+_installSymLink .icons ~/.icons ${WORKSPACE_DOTFILES}/dotfiles/gtk/.icons/ ~/
 
 echo "-------------------------------------"
 echo "-> Install Qtile dotfiles"
 echo "-------------------------------------"
 echo ""
 
-_installSymLink qtile ~/.config/qtile ~/dotfiles/qtile/ ~/.config
-_installSymLink polybar ~/.config/polybar ~/dotfiles/polybar/ ~/.config
-_installSymLink picom ~/.config/picom ~/dotfiles/picom/ ~/.config
-_installSymLink .xinitrc ~/.xinitrc ~/dotfiles/qtile/.xinitrc ~/.xinitrc
+_installSymLink qtile ~/.config/qtile ${WORKSPACE_DOTFILES}/dotfiles/qtile/ ~/.config
+_installSymLink polybar ~/.config/polybar ${WORKSPACE_DOTFILES}/dotfiles/polybar/ ~/.config
+_installSymLink picom ~/.config/picom ${WORKSPACE_DOTFILES}/dotfiles/picom/ ~/.config
+_installSymLink .xinitrc ~/.xinitrc ${WORKSPACE_DOTFILES}/dotfiles/qtile/.xinitrc ~/.xinitrc
 
 echo "-------------------------------------"
 echo "-> Install Hyprland dotfiles"
 echo "-------------------------------------"
 echo ""
 
-_installSymLink hypr ~/.config/hypr ~/dotfiles/hypr/ ~/.config
-_installSymLink waybar ~/.config/waybar ~/dotfiles/waybar/ ~/.config
-_installSymLink swaylock ~/.config/swaylock ~/dotfiles/swaylock/ ~/.config
-_installSymLink wlogout ~/.config/wlogout ~/dotfiles/wlogout/ ~/.config
-_installSymLink swappy ~/.config/swappy ~/dotfiles/swappy/ ~/.config
+_installSymLink hypr ~/.config/hypr ${WORKSPACE_DOTFILES}/dotfiles/hypr/ ~/.config
+_installSymLink waybar ~/.config/waybar ${WORKSPACE_DOTFILES}/dotfiles/waybar/ ~/.config
+_installSymLink swaylock ~/.config/swaylock ${WORKSPACE_DOTFILES}/dotfiles/swaylock/ ~/.config
+_installSymLink wlogout ~/.config/wlogout ${WORKSPACE_DOTFILES}/dotfiles/wlogout/ ~/.config
+_installSymLink swappy ~/.config/swappy ${WORKSPACE_DOTFILES}/dotfiles/swappy/ ~/.config
 
 # ------------------------------------------------------
 # DONE
